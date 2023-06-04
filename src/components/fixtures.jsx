7f3d583f3ec9cf2f33916fc8a0d72fc575aa6aed
leroyteojy <leroyteojy@gmail.com>
2023-06-05 00:23:31 +0800

    Feat: Added skeleton fixture displays
    Fixture page will now show the upcoming and finished fixtures.
    
    CSS to be modified.

--- src/components/fixtures.jsx
+++ src/components/fixtures.jsx
@@ -1,9 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./../fixtures.css";
 import TwelfthManLogo from "./../images/TwelfthManLogo.jpg";
 import { Link } from "react-router-dom";
 
 function FixturesPage() {
+  const [fixturesData, setFixturesData] = useState(null);
+  const [selectedLeague, setSelectedLeague] = useState("all");
+
+  useEffect(() => {
+    loadFixtures();
+  }, []);
+
+  const loadFixtures = () => {
+    fetch("https://damp-bayou-37411.herokuapp.com/fixtures")
+      .then((response) => response.json())
+      .then((data) => {
+        setFixturesData(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const createFixtureRows = (fixtures) => {
+    return fixtures.map((fixture) => {
+      const homeTeam = fixture.homeTeam.name;
+      const awayTeam = fixture.awayTeam.name;
+      const homeTeamCrest = fixture.homeTeam.crestUrl;
+      const awayTeamCrest = fixture.awayTeam.crestUrl;
+      const date = new Date(fixture.utcDate).toLocaleString();
+      const score =
+        fixture.status === "FINISHED"
+          ? `${fixture.score.fullTime.home} - ${fixture.score.fullTime.away}`
+          : "-";
+      const league = fixture.competition.name;
+
+      return (
+        <tr key={fixture.id}>
+          <td>
+            <img src={fixture.homeTeam.crest} alt={`${homeTeam} Crest`} />
+            {homeTeam}
+          </td>
+          <td>
+            <img src={fixture.awayTeam.crest} alt={`${awayTeam} Crest`} />
+            {awayTeam}
+          </td>
+          <td>{date}</td>
+          <td>{score}</td>
+          <td>{league}</td>
+        </tr>
+      );
+    });
+  };
+
+  const handleLeagueChange = (event) => {
+    setSelectedLeague(event.target.value);
+  };
+
+  const filterFixturesByLeague = (fixtures) => {
+    if (selectedLeague === "all") {
+      return fixtures;
+    }
+    return fixtures.filter(
+      (fixture) => fixture.competition.name === selectedLeague
+    );
+  };
+
+  const renderLeagueOptions = () => {
+    const leagues = [
+      "all",
+      "Premier League",
+      "Bundesliga",
+      "Primera Division",
+      "Serie A",
+      "Ligue 1",
+      "Campeonato Brasileiro Série A",
+    ];
+
+    return leagues.map((league) => {
+      return (
+        <option key={league} value={league}>
+          {league}
+        </option>
+      );
+    });
+  };
+
+  const renderTable = () => {
+    if (fixturesData) {
+      const headerText = "Upcoming Fixtures";
+      const filteredFixtures = filterFixturesByLeague(fixturesData.matches);
+
+      return (
+        <div>
+          <div className="league-filter">
+            <label htmlFor="league-select">Filter by league:</label>
+            <select
+              id="league-select"
+              value={selectedLeague}
+              onChange={handleLeagueChange}
+            >
+              {renderLeagueOptions()}
+            </select>
+          </div>
+          <h2 className="fixtures-table">{headerText}</h2>
+          <table id="fixtures-table">
+            <thead>
+              <tr>
+                <th>Home Team</th>
+                <th>Away Team</th>
+                <th>Date</th>
+                <th>Score</th>
+                <th>League</th>
+              </tr>
+            </thead>
+            <tbody>{createFixtureRows(filteredFixtures)}</tbody>
+          </table>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="fixtures-container">
       <div id="header" className="fixtures-header-container">
@@ -47,8 +165,8 @@ function FixturesPage() {
             </ul>
           </nav>
         </div>
+        <div className="center">{renderTable()}</div>
       </div>
-      {/* Add your fixtures content */}
     </div>
   );
 }
