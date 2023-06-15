ec99a8db7d34f80dfc5327ee59e34ff1f33e4741
nicholasteow <105709609+nicholasteow@users.noreply.github.com>
2023-06-16 03:23:09 +0800

    Feat: Skeleton for Predictions Selector

--- src/components/prediction.jsx
+++ src/components/prediction.jsx
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./../predictions.css";
 import TwelfthManLogo from "./../images/TwelfthManLogo.jpg";
 import { Link } from "react-router-dom";
+import Papa from "papaparse";
+import PremierLeagueData from "./../CSV/PLData.csv";
+import BundesligaData from "./../CSV/BL1Data.csv";
+import SerieAData from "./../CSV/SAData.csv";
+import PrimeraDivisionData from "./../CSV/PPLData.csv";
+import Ligue1 from "./../CSV/FL1Data.csv";
 
 function PredictionsPage() {
+  const [teamNames, setTeamNames] = useState([]);
+  const [homeTeam, setHomeTeam] = useState("");
+  const [awayTeam, setAwayTeam] = useState("");
+  const [selectedLeague, setSelectedLeague] = useState("");
+
+  const leagueOptions = [
+    { name: "Premier League", csvFile: PremierLeagueData },
+    { name: "Bundesliga", csvFile: BundesligaData },
+    { name: "Serie A", csvFile: SerieAData },
+    { name: "Primera Division", csvFile: PrimeraDivisionData },
+    { name: "Ligue 1", csvFile: Ligue1 },
+  ];
+
+  useEffect(() => {
+    if (selectedLeague) {
+      Papa.parse(selectedLeague, {
+        download: true,
+        header: true,
+        complete: function (results) {
+          const teamsData = results.data;
+          const uniqueTeamNames = getUniqueTeamNames(teamsData);
+          console.log(uniqueTeamNames);
+          setTeamNames(uniqueTeamNames);
+          }
+      });
+    }
+  }, [selectedLeague]);
+
+  function getUniqueTeamNames(teamsData) {
+    const uniqueNames = new Set();
+    teamsData.forEach((team) => {
+      uniqueNames.add(team.HomeTeam);
+      uniqueNames.add(team.AwayTeam);
+    });
+    return Array.from(uniqueNames)
+      .sort()
+      .filter((name) => name !== "" && name !== undefined);
+  }
+
+  function handleHomeTeamChange(event) {
+    const selectedTeam = event.target.value;
+    setHomeTeam(selectedTeam);
+  }
+
+  function handleAwayTeamChange(event) {
+    const selectedTeam = event.target.value;
+    setAwayTeam(selectedTeam);
+  }
+
+  function handleLeagueChange(event) {
+    const selectedLeague = event.target.value;
+    setSelectedLeague(selectedLeague);
+  }
+
   return (
     <div className="predictions-container">
       <div id="header" className="predictions-header-container">
         <div className="container">
           <nav>
             <img src={TwelfthManLogo} alt="TwelfthManLogo" />
-            <h2>Under Development!</h2>
             <ul>
               <li>
                 <Link to="/">Home</Link>
@@ -50,15 +109,69 @@ function PredictionsPage() {
                     <Link to="/standings?league=eredivisie">Eredivisie</Link>
                   </li>
                   <li>
-                    <Link to="/standings?league=campeonatobrasileiroseriea">Campeonato Brasileiro Série A</Link>
+                    <Link to="/standings?league=campeonatobrasileiroseriea">
+                      Campeonato Brasileiro Série A
+                    </Link>
                   </li>
                 </ul>
               </li>
             </ul>
           </nav>
         </div>
+
+          <div className="predictions-content">
+          <h2>Match Predictor</h2> {/* Add the heading "Match Predictor" */}
+          <div className="league-select">
+            <h3>Select League:</h3>
+            <select value={selectedLeague} onChange={handleLeagueChange}>
+              <option value="" disabled>
+                Select League
+              </option>
+              {leagueOptions.map((league, index) => (
+                <option key={index} value={league.csvFile}>
+                  {league.name}
+                </option>
+              ))}
+            </select>
+            </div>
+          <div className="team-select">
+            <div>
+              <h3>Select Home Team:</h3>
+              <select value={homeTeam} onChange={handleHomeTeamChange}>
+                <option value="">Home Team</option>
+                {teamNames.map((team, index) => {
+                  if (team !== awayTeam) {
+                    return (
+                      <option key={`home-team-${index}`} value={team}>
+                        {team}
+                      </option>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </select>
+            </div>
+            <div>
+              <h3>Select Away Team:</h3>
+              <select value={awayTeam} onChange={handleAwayTeamChange}>
+                <option value="">Away Team</option>
+                {teamNames.map((team, index) => {
+                  if (team !== homeTeam) {
+                    return (
+                      <option key={`away-team-${index}`} value={team}>
+                        {team}
+                      </option>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* Add your predictions content */}
     </div>
   );
 }
